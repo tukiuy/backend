ef5f6423e5bda1b6475bea0fb6da113a8afa8a0c
ignacio <tbadell@gmail.com>
2024-04-21 18:45:57 -0300

    Created db script for composer

--- src/Tuki.Transactions.Api/Program.cs
+++ src/Tuki.Transactions.Api/Program.cs
@@ -1,12 +1,52 @@
-﻿using Tuki.Transactions.Api.Controllers;
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Tuki.Common.Authentication.ApiKey;
+using Tuki.Transactions.Api.Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Api Key Auth",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "ApiKey must appear in header",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "XApiKey",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+                    {
+                             { key, new List<string>() }
+                    };
+    c.AddSecurityRequirement(requirement);
+});
+
+builder.Services.AddHealthChecks();
+builder.Services.AddApiKeyAuthentication();
+builder.Services.AddMediatR(c => c.RegisterServicesFromAssemblyContaining(typeof(Program)));
+builder.Services.AddDbContext<TransactionsContext>(opt => opt.UseSqlServer(config.GetConnectionString("defaul")));
 
 var app = builder.Build();
 
@@ -14,13 +54,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swaggerv1");
+    });
 }
 
+app.MapHealthChecks(
+    "/ArticulosHealth",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
